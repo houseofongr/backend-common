0b2011ced96abb7280baf838ebeea1fb86a5c42c
SangyeopNam <spearoad15@gmail.com>
2025-07-23 13:03:08 -0500

    feat : cache key 추가(Url <-> token)

--- src/main/java/com/hoo/common/enums/CacheKeys.java
+++ src/main/java/com/hoo/common/enums/CacheKeys.java
@@ -9,7 +9,8 @@ public enum CacheKeys {
     EMAIL_AUTHN_CODE_PREFIX("email-authn-code:"),
     EMAIL_AUTHN_STATUS_PREFIX("email-authenticated:"),
     EMAIL_AUTHENTICATED("1"),
-    FILE_TEMP_URL_PREFIX("temp-url:");
+    FILE_TEMP_URL_PREFIX("temp-url:"),
+    FILE_TOKEN_PREFIX("temp-token:");
 
 
     private final String key;
